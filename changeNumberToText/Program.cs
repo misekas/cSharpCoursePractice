23ec8f194166ae01aeea76a8aa01bb8866d4ddd1
litras <misekas@gmail.com>
2018-11-26 15:38:20 +0200

    Done!

--- changeNumberToText/Program.cs
+++ changeNumberToText/Program.cs
@@ -5,19 +5,15 @@ class Program
 
     static void Main()
     {
-        //Sveiki,
-        //    Dabar padariau tik minimuma, veliau pasistengsiu padaryti iki pabaigos.
-        //    Dvejose vietose pacheat'inau, bet siam momentui nesugalvojau kito budo. :)
-        //    Taip pat constantu reziu nebevedziau, nes tik paziurejes i jusu koda supratau, kad jas reiketu tureti. 
-        //    Norejau, kad paziuretumete pradini mano parasyma.
-
+        const int MaxValue = 19;
+        const int MinValue = -19;
         Console.WriteLine("Iveskite sveika skaiciu nuo -19 iki 19.");
         string ivedimas = Console.ReadLine();
-        if (ArSkaicius(ivedimas) == true)
+        if (ArSkaicius(ivedimas))
         {
             //Console.WriteLine($"Ivesta {ivedimas} reiksme yra skaicius.");
             int skaicius = Convert.ToInt32(ivedimas); 
-            if (PatikrintiRezi(skaicius) == true)
+            if (PatikrintiRezi(skaicius, MaxValue, MinValue))
             {
                 //Console.WriteLine($"Ivestas skaicius {ivedimas} teisinguose reziuose.");
                 Console.WriteLine($"Reiksme zodziais:\n{ArNeigiamas(skaicius)}"+ SkaiciaiZodziais(Math.Abs(skaicius)));
@@ -48,9 +44,9 @@ class Program
         return skaicius;
     }
 
-    static bool PatikrintiRezi(int skaicius)
+    static bool PatikrintiRezi(int skaicius, int MaxValue, int MinValue)
     {
-        if (skaicius >= -19 && skaicius <= 19)
+        if (skaicius >= MinValue && skaicius <= MaxValue)
         {
             return true;
         }
@@ -70,86 +66,122 @@ class Program
 
     static string SkaiciaiZodziais(int skaicius)
     {
+        if (skaicius < 10)
+        {
+            return SkaiciaiVienetai(skaicius);
+        }
 
-        if (skaicius == 0)
+        return " ";
+    }
+
+    static string SkaiciaiVienetai(int skaicius)
+    {
+        switch (skaicius)
         {
-            return "nulis";
+            case 0:
+                return "nulis";
+            case 1:
+                return "vienas";
+            case 2:
+                return "du";
+            case 3:
+                return "trys";
+            case 4:
+                return "keturi";
+            case 5:
+                return "penki";
+            case 6:
+                return "sesi";
+            case 7:
+                return "septyni";
+            case 8:
+                return "astuoni";
+            case 9:
+                return "devyni";
+            default:
+                return "Klaida";  
         }
-        else if (skaicius == 1)
-        {
-            return "vienas";
-        }
-        else if (skaicius == 2)
-        {
-            return "du";
-        }
-        else if (skaicius == 3)
-        {
-            return "trys";
-        }
-        else if (skaicius == 4)
-        {
-            return "keturi";
-        }
-        else if (skaicius == 5)
-        {
-            return "penki";
-        }
-        else if (skaicius == 6)
-        {
-            return "sesi";
-        }
-        else if (skaicius == 7)
-        {
-            return "septyni";
-        }
-        else if (skaicius == 8)
-        {
-            return "astuoni";
-        }
-        else if (skaicius == 9)
-        {
-            return "devyni";
-        }
-         else if (skaicius == 10)
-        {
-            return "desimt";
-        }
-        else if (skaicius == 11)
-        {
-            return "vienuolika";
-        }
-        else if (skaicius == 12)
-        {
-            return "dvylika";
-        }
-        else if (skaicius == 13)
-        {
-            return "trylika";
-        }
-        else if (skaicius == 14)
-        {
-            return "keturiolika";
-        }
-        else if (skaicius == 15)
-        {
-            return "penkiolika";
-        }
-        else if (skaicius == 16)
-        {
-            return "sesiolika";
-        }
-        else if (skaicius == 17)
-        {
-            return "septyniolika";
-        }
-        else if (skaicius == 18)
-        {
-            return "astuoniolika";
-        }
-        else 
-        {
-            return "devyniolika";
-        }
+         
+
+        //if (skaicius == 0)
+        //{
+        //    return "nulis";
+        //}
+        //else if (skaicius == 1)
+        //{
+        //    return "vienas";
+        //}
+        //else if (skaicius == 2)
+        //{
+        //    return "du";
+        //}
+        //else if (skaicius == 3)
+        //{
+        //    return "trys";
+        //}
+        //else if (skaicius == 4)
+        //{
+        //    return "keturi";
+        //}
+        //else if (skaicius == 5)
+        //{
+        //    return "penki";
+        //}
+        //else if (skaicius == 6)
+        //{
+        //    return "sesi";
+        //}
+        //else if (skaicius == 7)
+        //{
+        //    return "septyni";
+        //}
+        //else if (skaicius == 8)
+        //{
+        //    return "astuoni";
+        //}
+        //else if (skaicius == 9)
+        //{
+        //    return "devyni";
+        //}
+        //else if (skaicius == 10)
+        //{
+        //    return "desimt";
+        //}
+        //else if (skaicius == 11)
+        //{
+        //    return "vienuolika";
+        //}
+        //else if (skaicius == 12)
+        //{
+        //    return "dvylika";
+        //}
+        //else if (skaicius == 13)
+        //{
+        //    return "trylika";
+        //}
+        //else if (skaicius == 14)
+        //{
+        //    return "keturiolika";
+        //}
+        //else if (skaicius == 15)
+        //{
+        //    return "penkiolika";
+        //}
+        //else if (skaicius == 16)
+        //{
+        //    return "sesiolika";
+        //}
+        //else if (skaicius == 17)
+        //{
+        //    return "septyniolika";
+        //}
+        //else if (skaicius == 18)
+        //{
+        //    return "astuoniolika";
+        //}
+        //else
+        //{
+        //    return "devyniolika";
+        //}
     }
  }
--- practiceArrays/practiceArrays/Program.cs
+++ practiceArrays/practiceArrays/Program.cs
@@ -13,11 +13,16 @@ class Program
     static void GenerateNumber(int number)
     {
         int arrayNumber = number;
-        for (int i = 99999; i < 999998; i++)
+        for (int i = 0; i < 999999; i++)
         {
-          arrayNumber++;
-          NumberToArray(arrayNumber);
+            arrayNumber++;
+            if (arrayNumber > 999999)
+            {
+                break;
+            }
+            NumberToArray(arrayNumber);
         }
+
     }
 
     static void NumberToArray(int arrayNumber)
@@ -36,16 +41,16 @@ class Program
     static void CheckAreMembersDifferentInNumber(int[] membertoArray)
     {
         int[] isMembersDifferent = membertoArray.Clone() as int[];
-     // pameginti su for
+        // pameginti su for
         if (isMembersDifferent[0] != isMembersDifferent[1] && isMembersDifferent[0] != isMembersDifferent[2] && isMembersDifferent[0] != isMembersDifferent[3] && isMembersDifferent[0] != isMembersDifferent[4] && isMembersDifferent[0] != isMembersDifferent[5])
         {
-             if (isMembersDifferent[1] != isMembersDifferent[2] && isMembersDifferent[1] != isMembersDifferent[3] && isMembersDifferent[1] != isMembersDifferent[4] && isMembersDifferent[1] != isMembersDifferent[5])
+            if (isMembersDifferent[1] != isMembersDifferent[2] && isMembersDifferent[1] != isMembersDifferent[3] && isMembersDifferent[1] != isMembersDifferent[4] && isMembersDifferent[1] != isMembersDifferent[5])
             {
-                 if (isMembersDifferent[2] != isMembersDifferent[3] && isMembersDifferent[2] != isMembersDifferent[4] && isMembersDifferent[2] != isMembersDifferent[5])
+                if (isMembersDifferent[2] != isMembersDifferent[3] && isMembersDifferent[2] != isMembersDifferent[4] && isMembersDifferent[2] != isMembersDifferent[5])
                 {
-                     if (isMembersDifferent[3] != isMembersDifferent[4] && isMembersDifferent[3] != isMembersDifferent[5])
+                    if (isMembersDifferent[3] != isMembersDifferent[4] && isMembersDifferent[3] != isMembersDifferent[5])
                     {
-                         if (isMembersDifferent[4] != isMembersDifferent[5])
+                        if (isMembersDifferent[4] != isMembersDifferent[5])
                         {
                             string joinMembers = string.Join("", isMembersDifferent);
                             NumberMultiply(joinMembers, membertoArray);
@@ -61,175 +66,170 @@ class Program
     {
         int multiplyNumber = Convert.ToInt32(joinMembers);
         int multiply2 = multiplyNumber * 2;
-        if (multiply2 > 999999)
+        if (multiply2 < 999999)
         {
-            multiply2 = 0;
+            int multiply3 = multiplyNumber * 3;
+            if (multiply3 < 999999)
+            {
+                int multiply4 = multiplyNumber * 4;
+                if (multiply4 < 999999)
+                {
+                    int multiply5 = multiplyNumber * 5;
+                    if (multiply5 < 999999)
+                    {
+                        int multiply6 = multiplyNumber * 6;
+                        if (multiply6 < 999999)
+                        {
+                            MultiplyedNumberToArray(multiply2, multiply3, multiply4, multiply5, multiply6, membertoArray);
+                        }
+                    }
+                }
+            }
         }
-        int multiply3 = multiplyNumber * 3;
-        if (multiply3 > 999999)
-        {
-            multiply3 = 0;
-        }
-        int multiply4 = multiplyNumber * 4;
-        if (multiply4 > 999999)
-        {
-            multiply4 = 0;
-        }
-        int multiply5 = multiplyNumber * 5;
-        if (multiply5 > 999999)
-        {
-            multiply5 = 0;
-        }
-        int multiply6 = multiplyNumber * 6;
-        if (multiply6 > 999999)
-        {
-            multiply6 = 0;
-        }
-        MultiplyedNumberToArray(multiply2, multiply3, multiply4, multiply5, multiply6, membertoArray);
     }
 
-    static void MultiplyedNumberToArray(int multiply2 , int multiply3, int multiply4, int multiply5, int multiply6, int[] membertoArray)
+    static void MultiplyedNumberToArray(int multiply2, int multiply3, int multiply4, int multiply5, int multiply6, int[] membertoArray)
     {
-        int[] multiplyedToArray = new int[5] {multiply2 , multiply3, multiply4, multiply5, multiply6 };
+        int[] multiplyedToArray = new int[5] { multiply2, multiply3, multiply4, multiply5, multiply6 };
         foreach (int item in multiplyedToArray)
         {
             string stringMultiplyedNumber = Convert.ToString(item);
-            
+
             int[] multiplyedMemberToArray = new int[6];
             for (int i = 0; i < stringMultiplyedNumber.Length; i++)
             {
                 multiplyedMemberToArray[i] = int.Parse(Convert.ToString(stringMultiplyedNumber[i]));
             }
-            CheckArraysMembers(multiplyedMemberToArray, membertoArray);
+            if (!CheckArraysMembers(multiplyedMemberToArray, membertoArray))
+            {
+                return;
+            }
         }
+        Console.WriteLine(string.Join("", membertoArray));
     }
 
     static bool CheckArraysMembers(int[] multiplyedMemberToArray, int[] membertoArray)
     {
-        for (int i = 0; i < 5; i++)
+        int[][] arrayNumberSources = new int[][] { membertoArray, multiplyedMemberToArray };
+        if (membertoArray[0] != multiplyedMemberToArray[0])
         {
-
-            int[][] arrayNumberSources = new int[][] { membertoArray, multiplyedMemberToArray };
-            if (membertoArray[0] != multiplyedMemberToArray[0])
+            if (membertoArray[0] != multiplyedMemberToArray[1])
             {
-                if (membertoArray[0] != multiplyedMemberToArray[1])
+                if (membertoArray[0] != multiplyedMemberToArray[2])
                 {
-                    if (membertoArray[0] != multiplyedMemberToArray[2])
+                    if (membertoArray[0] != multiplyedMemberToArray[3])
                     {
-                        if (membertoArray[0] != multiplyedMemberToArray[3])
+                        if (membertoArray[0] != multiplyedMemberToArray[4])
                         {
-                            if (membertoArray[0] != multiplyedMemberToArray[4])
+                            if (membertoArray[0] != multiplyedMemberToArray[5])
                             {
-                                if (membertoArray[0] != multiplyedMemberToArray[5])
-                                {
-                                    return false;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            if (membertoArray[1] != multiplyedMemberToArray[0])
-            {
-                if (membertoArray[1] != multiplyedMemberToArray[1])
-                {
-                    if (membertoArray[1] != multiplyedMemberToArray[2])
-                    {
-                        if (membertoArray[1] != multiplyedMemberToArray[3])
-                        {
-                            if (membertoArray[1] != multiplyedMemberToArray[4])
-                            {
-                                if (membertoArray[1] != multiplyedMemberToArray[5])
-                                {
-                                    return false;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            if (membertoArray[2] != multiplyedMemberToArray[0])
-            {
-                if (membertoArray[2] != multiplyedMemberToArray[1])
-                {
-                    if (membertoArray[2] != multiplyedMemberToArray[2])
-                    {
-                        if (membertoArray[2] != multiplyedMemberToArray[3])
-                        {
-                            if (membertoArray[2] != multiplyedMemberToArray[4])
-                            {
-                                if (membertoArray[2] != multiplyedMemberToArray[5])
-                                {
-                                    return false;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            if (membertoArray[3] != multiplyedMemberToArray[0])
-            {
-                if (membertoArray[3] != multiplyedMemberToArray[1])
-                {
-                    if (membertoArray[3] != multiplyedMemberToArray[2])
-                    {
-                        if (membertoArray[3] != multiplyedMemberToArray[3])
-                        {
-                            if (membertoArray[3] != multiplyedMemberToArray[4])
-                            {
-                                if (membertoArray[3] != multiplyedMemberToArray[5])
-                                {
-                                    return false;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            if (membertoArray[4] != multiplyedMemberToArray[0])
-            {
-                if (membertoArray[4] != multiplyedMemberToArray[1])
-                {
-                    if (membertoArray[4] != multiplyedMemberToArray[2])
-                    {
-                        if (membertoArray[4] != multiplyedMemberToArray[3])
-                        {
-                            if (membertoArray[4] != multiplyedMemberToArray[4])
-                            {
-                                if (membertoArray[4] != multiplyedMemberToArray[5])
-                                {
-                                    return false;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            if (membertoArray[5] != multiplyedMemberToArray[0])
-            {
-                if (membertoArray[5] != multiplyedMemberToArray[1])
-                {
-                    if (membertoArray[5] != multiplyedMemberToArray[2])
-                    {
-                        if (membertoArray[5] != multiplyedMemberToArray[3])
-                        {
-                            if (membertoArray[5] != multiplyedMemberToArray[4])
-                            {
-                                if (membertoArray[5] != multiplyedMemberToArray[5])
-                                {
-                                    return false;
-                                }
+                                return false;
                             }
                         }
                     }
                 }
             }
         }
+        if (membertoArray[1] != multiplyedMemberToArray[0])
+        {
+            if (membertoArray[1] != multiplyedMemberToArray[1])
+            {
+                if (membertoArray[1] != multiplyedMemberToArray[2])
+                {
+                    if (membertoArray[1] != multiplyedMemberToArray[3])
+                    {
+                        if (membertoArray[1] != multiplyedMemberToArray[4])
+                        {
+                            if (membertoArray[1] != multiplyedMemberToArray[5])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (membertoArray[2] != multiplyedMemberToArray[0])
+        {
+            if (membertoArray[2] != multiplyedMemberToArray[1])
+            {
+                if (membertoArray[2] != multiplyedMemberToArray[2])
+                {
+                    if (membertoArray[2] != multiplyedMemberToArray[3])
+                    {
+                        if (membertoArray[2] != multiplyedMemberToArray[4])
+                        {
+                            if (membertoArray[2] != multiplyedMemberToArray[5])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (membertoArray[3] != multiplyedMemberToArray[0])
+        {
+            if (membertoArray[3] != multiplyedMemberToArray[1])
+            {
+                if (membertoArray[3] != multiplyedMemberToArray[2])
+                {
+                    if (membertoArray[3] != multiplyedMemberToArray[3])
+                    {
+                        if (membertoArray[3] != multiplyedMemberToArray[4])
+                        {
+                            if (membertoArray[3] != multiplyedMemberToArray[5])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (membertoArray[4] != multiplyedMemberToArray[0])
+        {
+            if (membertoArray[4] != multiplyedMemberToArray[1])
+            {
+                if (membertoArray[4] != multiplyedMemberToArray[2])
+                {
+                    if (membertoArray[4] != multiplyedMemberToArray[3])
+                    {
+                        if (membertoArray[4] != multiplyedMemberToArray[4])
+                        {
+                            if (membertoArray[4] != multiplyedMemberToArray[5])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (membertoArray[5] != multiplyedMemberToArray[0])
+        {
+            if (membertoArray[5] != multiplyedMemberToArray[1])
+            {
+                if (membertoArray[5] != multiplyedMemberToArray[2])
+                {
+                    if (membertoArray[5] != multiplyedMemberToArray[3])
+                    {
+                        if (membertoArray[5] != multiplyedMemberToArray[4])
+                        {
+                            if (membertoArray[5] != multiplyedMemberToArray[5])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        string joinMembers = string.Join("", membertoArray);
+        //Console.WriteLine(joinMembers);
         return true;
     }
-   
-   
-}
 
+}
 
 
